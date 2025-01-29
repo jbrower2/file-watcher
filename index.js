7fd8fdd22222632848690fc16dd8ea12cbf140d6
Jeff Brower <jeffbrower0@gmail.com>
2025-01-29 09:28:46 -0500

    Add usage example

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// node index.js "C:\Users\jeff\AppData\Local\Hogwarts Legacy\Saved\SaveGames\171551109" "HL-02-*" "Z:\hogwarts-saves"
+
 const crypto = require("crypto");
 const fs = require("fs");
 const glob = require("glob");
@@ -5,8 +7,8 @@ const minimatch = require("minimatch");
 const path = require("path");
 
 if (process.argv.length !== 5) {
-  console.error("Usage: node index.js <inputDir> <pattern> <outputDir>");
-  return;
+	console.error("Usage: node index.js <inputDir> <pattern> <outputDir>");
+	return;
 }
 
 const [, , inputDir, pattern, outputDir] = process.argv;
@@ -16,86 +18,86 @@ const HASH_FILE = "last.hash";
 let handle;
 
 function md5(f) {
-  const hash = crypto.createHash("md5");
-  hash.update(fs.readFileSync(f));
-  return hash.digest("hex");
+	const hash = crypto.createHash("md5");
+	hash.update(fs.readFileSync(f));
+	return hash.digest("hex");
 }
 
 function copyFiles() {
-  handle = undefined;
+	handle = undefined;
 
-  let hashFileContents;
-  try {
-    hashFileContents = fs.readFileSync(HASH_FILE, "utf8");
-  } catch (e) {}
-  const lastHashes = new Map(
-    hashFileContents
-      ? hashFileContents.split(/\r?\n/).map((line) => line.split("\t"))
-      : []
-  );
+	let hashFileContents;
+	try {
+		hashFileContents = fs.readFileSync(HASH_FILE, "utf8");
+	} catch (e) {}
+	const lastHashes = new Map(
+		hashFileContents
+			? hashFileContents.split(/\r?\n/).map((line) => line.split("\t"))
+			: []
+	);
 
-  const files = glob.sync(pattern, { cwd: inputDir });
+	const files = glob.sync(pattern, { cwd: inputDir });
 
-  const newHashes = new Map();
+	const newHashes = new Map();
 
-  let anyChanged = false;
-  for (const f of files) {
-    const hash = md5(`${inputDir}/${f}`);
-    newHashes.set(f, hash);
-    if (lastHashes.get(f) !== hash) {
-      anyChanged = true;
-    }
-  }
+	let anyChanged = false;
+	for (const f of files) {
+		const hash = md5(`${inputDir}/${f}`);
+		newHashes.set(f, hash);
+		if (lastHashes.get(f) !== hash) {
+			anyChanged = true;
+		}
+	}
 
-  if (!anyChanged) {
-    console.log("No changes");
-    return;
-  }
+	if (!anyChanged) {
+		console.log("No changes");
+		return;
+	}
 
-  fs.writeFileSync(
-    HASH_FILE,
-    Array.from(newHashes)
-      .map((e) => e.join("\t"))
-      .join("\n"),
-    "utf8"
-  );
+	fs.writeFileSync(
+		HASH_FILE,
+		Array.from(newHashes)
+			.map((e) => e.join("\t"))
+			.join("\n"),
+		"utf8"
+	);
 
-  console.log("Copying files");
+	console.log("Copying files");
 
-  const d = new Date();
-  const dir = [
-    outputDir,
-    "/",
-    d.getFullYear().toString(),
-    "-",
-    (d.getMonth() + 1).toString().padStart(2, "0"),
-    "-",
-    d.getDate().toString().padStart(2, "0"),
-    "T",
-    d.getHours().toString().padStart(2, "0"),
-    "_",
-    d.getMinutes().toString().padStart(2, "0"),
-    "_",
-    d.getSeconds().toString().padStart(2, "0"),
-  ].join("");
+	const d = new Date();
+	const dir = [
+		outputDir,
+		"/",
+		d.getFullYear().toString(),
+		"-",
+		(d.getMonth() + 1).toString().padStart(2, "0"),
+		"-",
+		d.getDate().toString().padStart(2, "0"),
+		"T",
+		d.getHours().toString().padStart(2, "0"),
+		"_",
+		d.getMinutes().toString().padStart(2, "0"),
+		"_",
+		d.getSeconds().toString().padStart(2, "0"),
+	].join("");
 
-  for (const f of files) {
-    const i = `${inputDir}/${f}`;
-    const o = `${dir}/${f}`;
+	for (const f of files) {
+		const i = `${inputDir}/${f}`;
+		const o = `${dir}/${f}`;
 
-    fs.mkdirSync(path.dirname(o), { recursive: true });
-    fs.cpSync(i, o);
-  }
+		fs.mkdirSync(path.dirname(o), { recursive: true });
+		fs.cpSync(i, o);
+	}
 }
 
 copyFiles();
 
 fs.watch(inputDir, { recursive: true }, (_, filename) => {
-  if (minimatch(filename, pattern)) {
-    console.log("Changed:", filename);
-    if (handle) {
-      clearTimeout(handle);
-    }
-    handle = setTimeout(copyFiles, 1000);
-  }
+	if (minimatch(filename, pattern)) {
+		console.log("Changed:", filename);
+		if (handle) {
+			clearTimeout(handle);
+		}
+		handle = setTimeout(copyFiles, 1000);
+	}
 });
